ad0230945476dbcc746ab09977aae1431e0e2226
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-06 11:31:17 +0300

    factorial function added

--- Program.py
+++ Program.py
@@ -1,9 +1,18 @@
+import math
+
+def factorial(number: float) -> float:
+    return math.factorial(number)
+
 def main():
     sum_1_to_100 = 0
-    for number in range(100):
+    for number in range(1, 101):
         sum_1_to_100 = sum_1_to_100 + number
 
     print(str(sum_1_to_100))
+
+    print(factorial(6.5))
+    print(factorial(7))
+    print(factorial(8))
 
 if __name__ == "__main__":
     main()
