d7baea3e58b4397d67dda5a64366ca5cf9759509
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-06 09:52:53 +0300

    Program finished

--- Program.py
+++ Program.py
@@ -1,5 +1,9 @@
 def main():
+    sum_1_to_100 = 0
+    for number in range(100):
+        sum_1_to_100 = sum_1_to_100 + number
 
+    print(str(sum_1_to_100))
 
 if __name__ == "__main__":
     main()
