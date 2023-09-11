3e403d6a7fbdfc75f82a9cb571d3fdb94efc3c7f
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-11 17:34:48 +0300

    function words length

--- advanced/advanced2/advanced.py
+++ advanced/advanced2/advanced.py
@@ -1,3 +1,13 @@
+def words_length(*words) -> None:
+    sum_length = 0
+
+    for item in words:
+        sum_length = sum_length + len(item)
+
+    print(f'The total length is {sum_length}')
+
+def total_age(**ages):
+
 def main():
 
 if __name__ == '__main__':
