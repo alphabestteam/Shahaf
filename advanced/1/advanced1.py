4575814248b502c835444d1dfb701fcd86378f77
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-11 15:37:33 +0300

    question 5 a+b

--- advanced/1/advanced1.py
+++ advanced/1/advanced1.py
@@ -1,5 +1,11 @@
-def main():
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
 
+def main():
+    point_one = Point(6, 8)
+    point_two = Point(6, 8)
 
 if __name__ == "__main__":
     main()
