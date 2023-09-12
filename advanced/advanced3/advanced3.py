3d35a479bb5317ea190c820e7f3d5f3128e740a5
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-12 11:27:44 +0300

    N series function

--- advanced/advanced3/advanced3.py
+++ advanced/advanced3/advanced3.py
@@ -7,7 +7,7 @@ class Person:
     def AddPerson(self, name: str) -> None:
         self.name_list.append(name)
 
-def timer(func: function) -> None:
+def timer(func) -> None:
      before = time.time()
 
      func()
@@ -16,12 +16,31 @@ def timer(func: function) -> None:
 
      print(f'The function run time was {after - before} seconds')
 
-def main():
+def hello() -> None:
+    print('Hello')
 
+def N_series(N: int) -> int:
+    series = [1, 2]
+    yield 1
+    yield 2
+
+    if N > 2:
+        for index in range(3, N + 1):
+            yield series[index-1-1] * series[index-2-1]
+
+def main():
+    run_time = timer(hello)
+    
     name_list = Person(['shahaf', 'shoham', 'yotam'])
     iter_name_list = iter(name_list.name_list)
     print(next(iter_name_list))
     print(next(iter_name_list))
+
+    N = int(input("Enter requested number of items: "))
+    series = N_series(N)
+    for item in series:
+        print(item)
+
 
 if __name__ == '__main__':
     main()
