9482d77cda03e23c77605e65941189ffd0d9e64c
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-12 10:53:00 +0300

    person class

--- advanced/advanced3/advanced3.py
+++ advanced/advanced3/advanced3.py
@@ -1,15 +1,27 @@
 import time
 
+class Person:
+    def __init__(self, name_list: list) -> None:
+        self.name_list = name_list
+
+    def AddPerson(self, name: str) -> None:
+        self.name_list.append(name)
+
 def timer(func: function) -> None:
-    before = time.time()
+     before = time.time()
 
-    func()
+     func()
 
-    after = time.time()
+     after = time.time()
 
-    print(f'The function run time was {after - before} seconds')
+     print(f'The function run time was {after - before} seconds')
 
 def main():
+
+    name_list = Person(['shahaf', 'shoham', 'yotam'])
+    iter_name_list = iter(name_list.name_list)
+    print(next(iter_name_list))
+    print(next(iter_name_list))
 
 if __name__ == '__main__':
     main()
