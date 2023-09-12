9f419aab6aececeeebc8599e3ca44b6c6c5d826a
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-09-12 10:44:23 +0300

    timer decorator

--- advanced/advanced3/advanced3.py
+++ advanced/advanced3/advanced3.py
@@ -1,3 +1,14 @@
+import time
+
+def timer(func: function) -> None:
+    before = time.time()
+
+    func()
+
+    after = time.time()
+
+    print(f'The function run time was {after - before} seconds')
+
 def main():
 
 if __name__ == '__main__':
