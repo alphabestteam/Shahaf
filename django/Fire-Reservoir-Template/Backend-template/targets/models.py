a92609e3f00877d4919e44951799c07ac3ddda48
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-13 09:52:30 +0300

    target model

--- django/Fire-Reservoir-Template/Backend-template/targets/models.py
+++ django/Fire-Reservoir-Template/Backend-template/targets/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 class Target(models.Model):
     # Implement here a target model with a __str__ function
+    name = models.CharField(max_length=100),
+    description = models.TextField(blank = False, null = False),
+    prioritization = models.CharField(max_length=1),
+    is_destroyed = models.BooleanField(blank = False, null = False),
+    x_coordinate = models.CharField(max_length=100),
+    y_coordinate = models.CharField(max_length=100),
+
+    def __str__(self) -> str:
+        return super().__str__()
