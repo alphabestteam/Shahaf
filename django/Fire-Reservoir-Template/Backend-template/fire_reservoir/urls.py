08a75735cc50b187b9f52748ee3d455fb5d64da4
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-13 09:33:06 +0300

    url

--- django/Fire-Reservoir-Template/Backend-template/fire_reservoir/urls.py
+++ django/Fire-Reservoir-Template/Backend-template/fire_reservoir/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Add here the targets URL
+    path("api/target/", include("target.urls")),
 ]
--- django/Fire-Reservoir-Template/Backend-template/targets/urls.py
+++ django/Fire-Reservoir-Template/Backend-template/targets/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Add here all the URLs you need
+    path("AddTarget/", views.add_target),
 ]
