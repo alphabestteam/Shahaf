a552ae8d7336e5afae638c39ce2464593e615c3a
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-13 10:06:02 +0300

    serializer

--- django/Fire-Reservoir-Template/Backend-template/targets/serializers.py
+++ django/Fire-Reservoir-Template/Backend-template/targets/serializers.py
@@ -3,12 +3,18 @@ from .models import Target
 
 class TargetSerializer(serializers.ModelSerializer):
     class Meta:
-        # define here your model and the fields to serialize
+        Model = Target
+        Fields = ('name', 'description', 'prioritization', 'is_destroyed', 'x_coordinate', 'y_coordinate')
 
     def create(self, validated_data):
         return Target(**validated_data)
     
     def update(self, instance, validated_data):
-        # Implement here an update function
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.prioritization = validated_data.get('prioritization', instance.prioritization)
+        instance.is_destroyed = validated_data.get('is_destroyed', instance.is_destroyed)
+        instance.x_coordinate = validated_data.get('x_coordinate', instance.x_coordinate)
+        instance.y_coordinate = validated_data.get('y_coordinate', instance.y_coordinate)
         return instance
     
