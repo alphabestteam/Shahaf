911fea88f0ae2a1e8bad30ec2a2fa018d5086cad
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-15 18:23:59 +0300

    add and all

--- django/Fire-Reservoir-Template/Backend-template/targets/models.py
+++ django/Fire-Reservoir-Template/Backend-template/targets/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Target(models.Model):
-    name = models.CharField(max_length=100, null= True)
-    attack_priority = models.CharField(max_length=2, null= True)
-    longitude = models.CharField(max_length = 100, null = True)
-    latitude = models.CharField(max_length = 100, null = True)
-    enemy_organization = models.CharField(max_length=100, null= True)
-    target_goal = models.CharField(max_length=100, null= True)
-    was_target_destroyed = models.BooleanField(blank = False, null= True)
-    target_id = models.IntegerField(null = False, primary_key = True)
+    name = models.CharField(max_length=100)
+    attack_priority = models.CharField(max_length=2)
+    longitude = models.CharField(max_length = 100)
+    latitude = models.CharField(max_length = 100)
+    enemy_organization = models.CharField(max_length=100)
+    target_goal = models.CharField(max_length=100)
+    was_target_destroyed = models.BooleanField(blank = False)
+    target_id = models.IntegerField(primary_key = True)
 
 
     def __str__(self) -> str:
