6fcd5f5166d24cb2195fb03109b843155d78e987
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-16 11:28:43 +0300

    changes

--- django/4/project4/people/views.py
+++ django/4/project4/people/views.py
@@ -10,6 +10,21 @@ def get_all_people(request):
 
 @csrf_exempt
 def add_person(request):
+    if request.method == 'POST':
+        request_data = JSONParser().parse(request)
+        person = Person(
+            name = request_data["name"],
+            date_of_birth = request_data["date_of_birth"],
+            city = request_data["city"],
+            id = request_data["id"]
+        )
+        serializer = PersonSerializer(data = request_data)
+        if serializer.is_valid():
+            person.save()
+            all_people = Person.objects.all()
+            
+            return JsonResponse(serializer.data, status = 200, safe= False)
+        return JsonResponse(serializer.errors, status = 400)
 
 @csrf_exempt
 def remove_person(request):
