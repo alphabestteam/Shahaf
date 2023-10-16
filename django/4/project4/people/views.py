a2c9203d17d0defd4df49db7daf2dc0c0500692c
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-16 11:58:35 +0300

    views

--- django/4/project4/people/views.py
+++ django/4/project4/people/views.py
@@ -7,6 +7,18 @@ from people.serializers import PersonSerializer
 
 @csrf_exempt
 def get_all_people(request):
+    all_people = Person.objects.all()
+    list_of_dict = []
+    for one_person in all_people.iterator():
+        serializer_person = PersonSerializer('json', one_person)
+        if serializer_person.is_valid():
+            list_of_dict.append(serializer_person)
+
+        else:
+            return JsonResponse(status = 404)
+        
+    return JsonResponse(list_of_dict, status = 200)
+
 
 @csrf_exempt
 def add_person(request):
@@ -21,13 +33,29 @@ def add_person(request):
         serializer = PersonSerializer(data = request_data)
         if serializer.is_valid():
             person.save()
-            all_people = Person.objects.all()
-            
-            return JsonResponse(serializer.data, status = 200, safe= False)
+            return JsonResponse(status = 200, safe= False)
         return JsonResponse(serializer.errors, status = 400)
 
 @csrf_exempt
-def remove_person(request):
+def remove_person(request, id):
+    if request.method == 'DELETE':
+        try:
+            person = Person.objects.get(id)
+            person.delete
+            return JsonResponse(status = 200)
+        
+        except:
+            return JsonResponse(status = 404)
 
 @csrf_exempt
 def update_person(request):
+    if request.method == 'POST':
+        data = JSONParser.parse(request)
+        person = Person.objects.get(name = data['id'])
+        person_serializer = PersonSerializer(data, person)
+        if person_serializer.is_valid():
+            person.save()
+            return JsonResponse(status = 200)
+        
+        else:
+            return JsonResponse(status = 404)
