b27e33db5c47d52978d0f47c88b108cd5ebede41
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-16 09:23:46 +0300

    person model

--- django/4/project4/people/models.py
+++ django/4/project4/people/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    city = models.CharField(max_length = 50)
+    id = models.CharField(max_length = 9, primary_key = True)
+
+
+    def __str__(self) -> str:
+        return f'name: {self.name} date of birth: {self.date_of_birth} city: {self.city} id: {self.id}'
