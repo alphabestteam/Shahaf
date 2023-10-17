04d1de25a1e7f5f49dbfa9bacf95592175e9c6b3
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-17 11:41:10 +0300

    parent model

--- django/5/project4/people/models.py
+++ django/5/project4/people/models.py
@@ -9,3 +9,9 @@ class Person(models.Model):
 
     def __str__(self) -> str:
         return f'name: {self.name} date of birth: {self.date_of_birth} city: {self.city} id: {self.id}'
+
+
+class Parent(Person):
+    place_of_work = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits = 8, decimal_places = 2)
+    children = models.ManyToManyField(Person)
