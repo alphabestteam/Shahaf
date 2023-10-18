d587056a8112324e2c9b7fa04ef8437a4b7f692b
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-18 17:02:07 +0300

    start

---
+++ django/6/project4/people/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("getAllPeople/", views.get_all_people),
+    path("addPerson/", views.add_person),
+    path("removePerson/<int:id>/", views.remove_person),
+    path("updatePerson/", views.update_person),
+    path("addParent/", views.add_parent),
+    path("removeParent/<int:id>/", views.remove_parent),
+    path("updateParent/", views.update_parent),
+    path("getAllParents/", views.get_all_parents),
+    path("setChildToParent/", views.set_child),
+    path("getInformation/<int:id>/", views.get_information),
+    path("richChildren/", views.rich_children),
+    path("findParents/<int:id>/", views.find_parents),
+    path("findParentsSerializer/<int:id>/", views.find_parents_serializer),
+    path("informationChildren/<int:id>/", views.information_children),
+    path("findGrandparents/<int:id>/", views.find_grandparents),
+    path("findSiblings/<int:id>/", views.find_siblings),
+]
---
+++ django/6/project4/people/views.py
@@ -0,0 +1,228 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from people.models import Person, Parent
+from people.serializers import PersonSerializer, ParentSerializer
+import json
+from rest_framework import status
+
+@csrf_exempt
+def get_all_people(request):
+    if request.method == 'GET':
+        all_people = Person.objects.all()
+        list_of_dict = []
+        for one_person in all_people.iterator():
+            try:
+                one_person = one_person.__dict__
+                list_of_dict.append(one_person)
+
+            except:
+                return HttpResponse(one_person.errors, status = 404)
+            
+        return HttpResponse(list_of_dict, status = 200)
+
+
+@csrf_exempt
+def add_person(request):
+    if request.method == 'POST':
+        request_data = JSONParser().parse(request)
+        person = Person(
+            name = request_data["name"],
+            date_of_birth = request_data["date_of_birth"],
+            city = request_data["city"],
+            id = request_data["id"]
+        )
+        serializer = PersonSerializer(data = request_data)
+        if serializer.is_valid():
+            person.save()
+            return JsonResponse(serializer.data, status = 200, safe= False)
+        return JsonResponse(serializer.errors, status = 400)
+
+@csrf_exempt
+def remove_person(request, id):
+    if request.method == 'DELETE':
+        try:
+            person = Person.objects.get(id = id)
+            person.delete()
+            return HttpResponse('deleted', status = 200)
+            
+        except:
+            return HttpResponse('error', status = 404)
+
+@csrf_exempt
+def update_person(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        person = Person.objects.get(id = data['id'])
+        print(person)
+        person_serializer = PersonSerializer(person, data)
+        if person_serializer.is_valid():
+            person_serializer.update(person, data)
+            return JsonResponse(person_serializer.data, status = 200)
+        
+        else:
+            return JsonResponse(person_serializer.errors, status = 404)
+
+#5 section
+
+@csrf_exempt  #checked
+def add_parent(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ParentSerializer(data=data)
+        if serializer.is_valid():
+            parent = parent = Parent(name = data['name'], date_of_birth = data['date_of_birth'], city = data['city'], id = data['id'], place_of_work = data['place_of_work'], salary = data['salary'])
+            parent.save()
+            parent.children.set(data.get('children', []))
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def remove_parent(request, id):
+    if request.method == 'DELETE':
+        try:
+            parent = Parent.objects.get(id = id)
+            parent.delete()
+            return HttpResponse('deleted', status=status.HTTP_204_NO_CONTENT)
+            
+        except:
+            return HttpResponse('error', status=status.HTTP_404_NOT_FOUND)
+
+@csrf_exempt
+def update_parent(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        parent = Parent.objects.get(id = data['id'])
+        parent_serializer = ParentSerializer(parent, data)
+        if parent_serializer.is_valid():
+            parent_serializer.save()
+            return JsonResponse(parent_serializer.data, status = 200)
+        
+        else:
+            return JsonResponse(parent_serializer.errors, status = 404)
+
+@csrf_exempt
+def get_all_parents(request):
+    if request.method == 'GET':
+        all_parents = Parent.objects.all()
+        list_of_dict = []
+        for one_parent in all_parents.iterator():
+            try:
+                one_parent = one_parent.__dict__
+                list_of_dict.append(one_parent)
+
+            except:
+                return HttpResponse(one_parent.errors, status = 404)
+            
+        return HttpResponse(list_of_dict, status = 200)
+
+@csrf_exempt
+def set_child(request):
+    if request.method == 'POST':
+        try:
+            dict_id = JSONParser().parse(request)
+            parent = Parent.objects.get(id = dict_id["parent_id"])
+            child = Person.objects.get(id = dict_id["child_id"])
+            if child not in parent.children.all():
+                parent.children.add(child)
+                return HttpResponse('connected child to parent', status = 200)
+            
+            else:
+                return HttpResponse('child already added', status = 404)
+        
+        except:
+            return HttpResponse('error', status = 404)
+
+@csrf_exempt
+def get_information(request, id):
+    if request.method == 'GET':
+        try:
+            parent = Parent.objects.get(id = id)
+            return HttpResponse(parent, status = 200)
+
+        except:
+            return HttpResponse(parent, status = 404)
+        
+@csrf_exempt
+def rich_children(request):
+    if request.method == 'GET':
+        all_parents = Parent.objects.all()
+        list_of_rich = []
+        for one_parent in all_parents.all():
+            try:
+                if one_parent.salary >= 50000.00:
+                    children = one_parent.children.all()
+                    if children not in list_of_rich:
+                        list_of_rich.append(children)
+
+            except:
+                return HttpResponse(one_parent.errors, status = 404)
+            
+        return HttpResponse(list_of_rich, status = 200)
+    
+@csrf_exempt
+def find_parents(request, id):
+    if request.method == 'GET':
+        all_parents = Parent.objects.all()
+        list_of_parents = []
+        for one_parent in all_parents.all():
+            try:
+                for child in one_parent.children.all():
+                    if int(child.id) == int(id):
+                        list_of_parents.append(one_parent)
+
+            except:
+                return HttpResponse(one_parent, status = 404)
+            
+        return HttpResponse(list_of_parents, status = 200)
+    
+@csrf_exempt
+def find_parents_serializer(request, id):  #fix
+    if request.method == 'GET':
+        try:
+            person = Person.objects.get(id = id)
+            parents = person.parents.all()
+            serializer = ParentSerializer(parents)
+            return HttpResponse(parents, status = 200)
+        
+        except:
+            return HttpResponse(status = 404)
+    
+@csrf_exempt
+def information_children(request, id):
+    if request.method == 'GET':
+        try:
+            parent = Parent.objects.get(id = id)
+            children = parent.children.all()
+            return HttpResponse(children, status = 200)
+            
+        except:
+            return HttpResponse('cant find parent', status = 404)
+        
+@csrf_exempt
+def find_grandparents(request, id):
+    if request.method == 'GET':
+        try:
+            person = Person.objects.get(id = id)
+            all_parents = person.parents.all()
+            grandparents = []
+            for parent in all_parents:
+                grandparents.append(parent.parents.all())
+
+            return HttpResponse(grandparents, status = 200)
+
+        except:
+            return HttpResponse(grandparents, status = 404)
+
+@csrf_exempt
+def find_siblings(request, id):
+    if request.method == 'GET':
+        try:
+            person = Person.objects.get(id = id)
+            parent = person.parents.first()
+            siblings = parent.children.all()
+            return HttpResponse(siblings, status = 200)
+
+        except:
+            return HttpResponse(status = 404)
