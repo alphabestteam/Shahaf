0a3ef3f5b7dc760608ce30d610a552c78b89b597
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-19 17:50:34 +0300

    first test works

--- django/8/project4/people/models.py
+++ django/8/project4/people/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+import datetime
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
@@ -11,6 +12,13 @@ class Person(models.Model):
     def __str__(self) -> str:
         return f'name: {self.name} date of birth: {self.date_of_birth} city: {self.city} id: {self.id}'
 
+    def is_18(self):
+        current_date = datetime.datetime.now()
+        current_year = current_date.year
+        if current_year - self.date_of_birth.year >= 18:
+            return True
+        else:
+            return False
 
 class Parent(Person):
     place_of_work = models.CharField(max_length=50)
--- django/8/project4/people/tests.py
+++ django/8/project4/people/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Person, Parent
 
-# Create your tests here.
+class PersonTestCase(TestCase):
+    def setUp(self):
+        Person.objects.create(name = 'shahaf', date_of_birth = '2004-10-23', city = 'Netaim', id = '214653602')
+
+    def test_is_18(self):
+        person = Person.objects.get(id = '214653602')
+        result = person.is_18()
+        self.assertEqual(result, True)
