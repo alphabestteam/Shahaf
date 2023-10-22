dcb6a10a62015aefe9b6988a03d24bcdf1cd75b9
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-10-22 16:10:22 +0300

    model books

---
+++ drf/1/myProject/books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'title: {self.title} author: {self.author} publication date: {self.publication_date} description: {self.description}'
