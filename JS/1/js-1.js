6e15ac10eedd1a9c345a1c62468961e3c32175d5
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-05 11:35:14 +0200

    all questions work

--- JS/1/js-1.js
+++ JS/1/js-1.js
@@ -1,1 +1,17 @@
 alert("Patrick went missing, go find him!")
+
+if (confirm("Are you going to find him now?") == true){
+    alert("Spongebob is going to find Patrik!")
+}
+
+let answer = prompt("Did you find Patrik?")
+
+while (answer != null){
+    if (answer == 'yes'){
+        alert("Spongebob found Patrik!")
+        break
+    }
+    else{
+        answer = prompt("Did you find Patrik?")
+    }
+}
