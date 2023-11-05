5751728bab19b5f241f90a5ecc601e1ffd79a21a
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-05 13:57:06 +0200

    start

---
+++ JS/4/script.js
@@ -0,0 +1,24 @@
+/*
+Your job for today is to finish the starSign function.
+
+Find the astrological sign, given the birth details as a Date object.
+Start and end dates for zodiac signs very on different resources, 
+so we will use this table to get consistent testable results:
+
+Aquarius ----- 21 January - 19 February
+Pisces ----- 20 February - 20 March
+Aries ----- 21 March - 20 April
+Taurus ----- 21 April - 21 May
+Gemini ----- 22 May - 21 June
+Cancer ----- 22 June - 22 July
+Leo ----- 23 July - 23 August
+Virgo ----- 24 August - 23 September
+Libra ----- 24 September - 23 October
+Scorpio ------ 24 October - 22 November
+Sagittarius ----- 23 November - 21 December
+Capricon ------ 22 December - 20 January
+*/
+
+function starSign(date){
+    return "Implement this function as described. I expect clean readable code. Good luck!"
+}
