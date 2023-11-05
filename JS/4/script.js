d9d69b6b620611ba19092815373c2b0baa70b9fa
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-05 14:20:12 +0200

    starSign function works

--- JS/4/script.js
+++ JS/4/script.js
@@ -19,6 +19,112 @@ Sagittarius ----- 23 November - 21 December
 Capricon ------ 22 December - 20 January
 */
 
+
 function starSign(date){
-    return "Implement this function as described. I expect clean readable code. Good luck!"
+    let month = date.getMonth()
+    let day = date.getDate()
+
+    switch(month){
+        case 0:
+            if (day <= 20){
+                return "Capricon"
+            }
+            else{
+                return "Aquarius"
+            }
+
+        case 1:
+            if (day <= 19){
+                return "Aquarius"
+            }
+            else{
+                return "Pisces"
+            }
+
+        case 2:
+            if (day <= 20){
+                return "Pisces"
+            }
+            else{
+                return "Aries"
+            }
+
+        case 3:
+            if (day <= 20){
+                return "Aries"
+            }
+            else{
+                return "Taurus"
+            }
+
+        case 4:
+            if (day <= 21){
+                return "Taurus"
+            }
+            else{
+                return "Gemini"
+            }
+
+        case 5:
+            if (day <= 21){
+                return "Gemini"
+            }
+            else{
+                return "Cancer"
+            }
+
+        case 6:
+            if (day <= 22){
+                return "Cancer"
+            }
+            else{
+                return "Leo"
+            }
+
+        case 7:
+            if (day <= 23){
+                return "Leo"
+            }
+            else{
+                return "Virgo"
+            }
+
+        case 8:
+            if (day <= 23){
+                return "Virgo"
+            }
+            else{
+                return "Libra"
+            }
+
+        case 9:
+            if (day <= 23){
+                return "Libra"
+            }
+            else{
+                return "Scorpio"
+            }
+
+        case 10:
+            if (day <= 22){
+                return "Scorpio"
+            }
+            else{
+                return "Sagittarius"
+            }
+
+        case 11:
+            if (day <= 21){
+                return "Sagittarius"
+            }
+            else{
+                return "Capricon"
+            }
+    }
 }
+
+const date = new Date("2022-03-25")
+
+let answer = starSign(date)
+
+console.log(answer)
