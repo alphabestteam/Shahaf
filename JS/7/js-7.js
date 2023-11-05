72a9a0bdd62272e3789ee4081583f317c6421fa1
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-05 18:09:48 +0200

    everything works

---
+++ JS/7/js-7.js
@@ -0,0 +1,15 @@
+const spongebob_friends = ['sandy', 'patrick', 'squidward', 'Mr.krab', 'Gary'];
+
+console.log(spongebob_friends);
+console.log(spongebob_friends.length);
+
+spongebob_friends.push('Mrs.puff');
+
+console.log(spongebob_friends);
+console.log(spongebob_friends.length);
+
+spongebob_friends[0] = 'Pearl';
+
+console.log(spongebob_friends);
+
+// the const defines a constant reference to an array, an object of type array
