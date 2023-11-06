773d960917fe869967b0a3d686bf23c9c7063691
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-06 18:57:34 +0200

    everything works

--- JS/15/files to use/script.js
+++ JS/15/files to use/script.js
@@ -3,5 +3,17 @@ const main = document.querySelector("main");
 const bobGif = document.getElementById("bob");
 
 const toggleBob = function(){
-    //implement this function
+    if (button.name == "notClicked"){
+        bobGif.setAttribute("src", "./assets/sbdance.gif")
+        button.innerText = "Hide Bob ;)"
+        button.name = "clicked"
+    }
+
+    else if (button.name == "clicked"){
+        bobGif.setAttribute("src", "")
+        button.innerText = "Show Me Bob ;)"
+        button.name = "notClicked"
+    }
 };
+
+button.addEventListener("click", toggleBob)
