2c8301d6eb66d3ae9569676aba631e24d45f8621
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-06 18:24:09 +0200

    start of work

---
+++ JS/15/files to use/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('the-button');
+const main = document.querySelector("main");
+const bobGif = document.getElementById("bob");
+
+const toggleBob = function(){
+    //implement this function
+};
