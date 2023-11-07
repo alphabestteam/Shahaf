02e4e383dd71016f01dc6e02d92f1add32da55f0
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-07 10:13:49 +0200

    finished building fishing advanture and it works

--- JS/18/files to use/script.js
+++ JS/18/files to use/script.js
@@ -33,22 +33,53 @@ const jellyfishDays = [
 
 // SpongeBob's net callback function
 function catchJellyfish(jellyfish, identifyJellyfishAndAddPoints) {
-    
+    return identifyJellyfishAndAddPoints(jellyfish, addPoints);
 }
 
 // Patrick's net callback function
 function identifyJellyfishAndAddPoints(jellyfish, addPoints) {
-    
+    let specie = identifySpecies(jellyfish);
+    return addPoints(specie);
+
 }
 
 // Score keeping callback function
 function addPoints(species) {
-    
+    if (species != 'common'){
+        return speciesPoints[species];
+    }
+    return 0;
 }
 
 // Helper functions
 function identifySpecies(jellyfish) {
-    
+    if (jellyfish == 'pink'){
+        return 'pink spotted';
+    }
+    else if (jellyfish == 'blue'){
+        return 'blue stinger';
+    }
+    else if (jellyfish == 'blue'){
+        return 'green itches';
+    }
+    else{
+        return 'common';
+    }
 }
 
 //The Adventure Starts Here! 
+
+for (let i = 0; i < jellyfishDays.length; i++){
+    console.log("Let's go jellyfishing!");
+    const jellyfishesArr = jellyfishDays[i];
+    let score = 0;
+
+    for (let jellyfish = 0; jellyfish < jellyfishDays.length; jellyfish++){
+        console.log(`SpongeBob caught a ${jellyfishesArr[jellyfish].color} jellyfish!`);
+        score += catchJellyfish(jellyfishesArr[jellyfish].color, identifyJellyfishAndAddPoints);
+        console.log(`score: ${score}`);
+    }
+
+    console.log('Great job, SpongeBob and Patrick!');
+    console.log(`Final score: ${score}`)
+}
