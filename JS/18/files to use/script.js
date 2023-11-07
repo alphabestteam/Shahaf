49654f66b37edb86b615250e8f5c86fefba8c054
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-07 09:38:48 +0200

    files at start of work

---
+++ JS/18/files to use/script.js
@@ -0,0 +1,54 @@
+const speciesPoints = {
+    'pink spotted': 4,
+    'blue stinger': 3,
+    'green itches': 2
+};
+
+const jellyfishDays = [
+    [
+        { color: 'pink'},
+        { color: 'pink'},
+        { color: 'blue'},
+        { color: 'green'},
+        { color: 'white'},
+        { color: 'white'},
+    ],
+    [
+        { color: 'pink'},
+        { color: 'pink'},
+        { color: 'blue'},
+        { color: 'green'},
+        { color: 'green'},
+        { color: 'green'},
+    ],
+    [
+        { color: 'pink'},
+        { color: 'pink'},
+        { color: 'pink'},
+        { color: 'pink'},
+        { color: 'blue'},
+        { color: 'green'},
+    ]
+];
+
+// SpongeBob's net callback function
+function catchJellyfish(jellyfish, identifyJellyfishAndAddPoints) {
+    
+}
+
+// Patrick's net callback function
+function identifyJellyfishAndAddPoints(jellyfish, addPoints) {
+    
+}
+
+// Score keeping callback function
+function addPoints(species) {
+    
+}
+
+// Helper functions
+function identifySpecies(jellyfish) {
+    
+}
+
+//The Adventure Starts Here! 
