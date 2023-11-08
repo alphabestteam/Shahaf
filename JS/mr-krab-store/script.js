8ce6d1983ce3d7281c820303be11b418ebde0494
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-08 13:15:52 +0200

    start of work

---
+++ JS/mr-krab-store/script.js
@@ -0,0 +1,9 @@
+
+/*
+List of endpoints:
+  GET - http://localhost:8000/hello -> {'Hello': 'World'} Here as an example
+  GET - http://localhost:8000/menu -> {'items': menu} A dict of the menu
+  POST - http://localhost:8000/latest-order -> A dict of the latest order
+  POST - http://localhost:8000/orders -> An endpoint to handle an order. The order is in the http body as so: { 'items': items }
+
+*/
