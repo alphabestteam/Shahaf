37fb6168529f1ba0f708ad2177c3b50554a435d9
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-20 16:39:42 +0200

    all works

---
+++ angular/basics-assignment-1-start 3/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isSuccess = true
+
+  options: number[] = [1, 2, 3];
+  selectedOption: number = this.options[0];
+
+  showWarning(): void {
+    this.isSuccess = false
+  }
+
+  showSuccess(): void {
+    this.isSuccess = true
+  }
+
+  getRepeatArray(count: number): number[] {
+    return Array.from({ length: count }, (_, i) => i);
+  }
+}
