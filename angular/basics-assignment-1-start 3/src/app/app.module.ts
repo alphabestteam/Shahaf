9c7169d6a148580ca42f3d1e83b30cd2b6d57c71
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-20 16:25:37 +0200

    angular metirial buttons

--- angular/basics-assignment-1-start 3/src/app/app.module.ts
+++ angular/basics-assignment-1-start 3/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
