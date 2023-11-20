9504496019acfeff56b1ca003f7e3d74a44a01cd
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-20 16:02:04 +0200

    range works

--- angular/basics-assignment-1-start 3/src/app/app.component.ts
+++ angular/basics-assignment-1-start 3/src/app/app.component.ts
@@ -8,11 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isSuccess = true
 
+  options: number[] = [1, 2, 3];
+  selectedOption: number = this.options[0];
+
   showWarning(): void {
     this.isSuccess = false
   }
 
   showSuccess(): void {
     this.isSuccess = true
+  }
+
+  getRepeatArray(count: number): number[] {
+    return Array.from({ length: count }, (_, i) => i);
   }
 }
