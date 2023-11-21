e71cf8a7cf0c3cfa67dadf08f839b1c9dded5bdf
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-21 10:13:43 +0200

    all works with ngstyle and ngclass

--- basics-assignment-3-start 3/src/app/app.component.ts
+++ basics-assignment-3-start 3/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  paragraph = 'Secret Password = tuna';
+  click = 0;
+  showContent = true;
+
+  onClick(): void{
+    this.showContent = !this.showContent;
+    this.click += 1;
+
+  }
+
+  getRepeatArray(count: number): number[] {
+    return Array.from({ length: count }, (_, i) => i);
+  }
+
+  setBackgroundColor(): string {
+    return 'blue';
+  }
 }
