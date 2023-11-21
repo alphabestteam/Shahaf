66deb22b022e0df117a666f8816388d0e4195a8d
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-21 10:19:25 +0200

    start of work

---
+++ basics-assignments-4-start 2/src/app/my-inner/my-inner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-inner',
+  templateUrl: './my-inner.component.html',
+  styleUrls: ['./my-inner.component.css']
+})
+export class MyInnerComponent {
+
+}
