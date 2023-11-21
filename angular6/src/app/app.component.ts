c6cf9bed51fc847d99fde06d5e2950b8bd093657
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-21 18:49:39 +0200

    form works

--- angular6/src/app/app.component.ts
+++ angular6/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular6';
+  
+  userForm: FormGroup = new FormGroup({});;
+  formError = this.userForm.valid;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  outputForm() {
+    if (this.formError) {
+      console.log(this.userForm.value)
+    }
+    else {
+      console.log(this.userForm.errors)
+    }
+  }
 }
