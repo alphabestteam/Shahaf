21f2a91100c24d6f114ce473391f5ff36c751ca2
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-22 13:26:02 +0200

    fixed comments

--- angular6/src/app/app.component.ts
+++ angular6/src/app/app.component.ts
@@ -7,10 +7,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'angular6';
+  title: string = 'angular6';
   
   userForm: FormGroup = new FormGroup({});;
-  formError = this.userForm.valid;
+  formError: boolean = this.userForm.valid;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -22,12 +22,24 @@ export class AppComponent implements OnInit {
     });
   }
 
-  outputForm() {
+  outputForm(): void {
     if (this.formError) {
       console.log(this.userForm.value)
     }
     else {
       console.log(this.userForm.errors)
     }
+  }
+
+  usernameIsTouched(): boolean {
+    return (this.userForm.get('username')?.invalid && (this.userForm.get('username')?.touched || this.userForm.get('username')?.dirty))??false
+  }
+
+  passwordIsTouched(): boolean {
+    return (this.userForm.get('password')?.invalid && (this.userForm.get('password')?.touched || this.userForm.get('password')?.dirty))??false
+  }
+
+  emailIsTouched(): boolean {
+    return (this.userForm.get('email')?.invalid && (this.userForm.get('email')?.touched || this.userForm.get('email')?.dirty))??false
   }
 }
