fb725bfd2be69eb1e9422aa106f0bc1ee70b378d
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-12-12 17:02:43 +0200

    settings

--- finalProject/finalProject/settings.py
+++ finalProject/finalProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'comments',
+    'recipes',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
