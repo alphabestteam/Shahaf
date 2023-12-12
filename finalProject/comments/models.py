6799f652599dbf67e374d516e1385c8ea69bfc63
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-12-12 16:56:55 +0200

    comment model

--- finalProject/comments/models.py
+++ finalProject/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users import User
+from recipes import Recipe
 
-# Create your models here.
+class Comment(models.Model):
+    user_id = models.OneToOneField(User, related_name= 'user_id', on_delete=models.CASCADE, blank = False)
+    text = models.TextField(blank= False)
+    stars = models.PositiveIntegerField(blank= False, limit_value = 5)
+    recipe_id = models.OneToOneField(Recipe, related_name= 'recipe_id', on_delete=models.CASCADE, blank = False)
+    comment_id = models.AutoField(primary_key= True)
